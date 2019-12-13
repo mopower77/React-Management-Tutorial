b1b063ac9ac31881d88b72806b1bc32899396c76
mopower77 <mopower77@gmail.com>
2019-12-14 01:07:42 +0900

    -머테리얼 npm 을 이용한 css 및 express를 이용한  서버 데이터 연동

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+app.use('/api/customers',(req,res)=>{
+    res.send([
+        {
+          id : 1,
+          image : 'https://placeimg.com/64/64/1',
+          name : '홍길동',
+          birthday : '991222',
+          gender : '남자',
+          job : '대학생'
+        } ,
+        {
+          id : 2,
+          image : 'https://placeimg.com/64/64/2',
+          name : '홍길동',
+          birthday : '991222',
+          gender : '남자',
+          job : '대학생'
+        } ,
+        {
+          id : 3,
+          image : 'https://placeimg.com/64/64/3',
+          name : '홍길동',
+          birthday : '991222',
+          gender : '남자',
+          job : '대학생'
+        } ,
+        {
+          id : 4,
+          image : 'https://placeimg.com/64/64/4',
+          name : '홍길동',
+          birthday : '991222',
+          gender : '남자',
+          job : '대학생'
+        } ,  {
+          id : 5,
+          image : 'https://placeimg.com/64/64/5',
+          name : '홍길동',
+          birthday : '991222',
+          gender : '남자',
+          job : '대학생'
+        } 
+        
+      ])
+})
+
+
+app.listen(port,()=>{
+    console.log(`Listening on port ${port}`);
+})
